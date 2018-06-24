c3c51e515aa5bbfe4eee4101e8f0520d438e1266
root <root@localhost.localdomain>
2018-06-25 02:22:27 +0300

    CI

--- Jenkins/CI_job.groovy
+++ Jenkins/CI_job.groovy
@@ -34,8 +34,8 @@ node {
 	stage('Push to dockerhub') {
 		withCredentials([usernamePassword(credentialsId: 'dockerHubAccount', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
 			sh "docker login -u $DOCKER_HUB_USER -p $PASSWORD"
-			sh "docker tag $CONTAINER_NAME:$CONTAINER_TAG $IMAGE_NAME"
-			sh "docker push $IMAGE_NAME"
+			sh "docker tag $CONTAINER_NAME:$CONTAINER_TAG $DOCKER_HUB_USER/$CONTAINER_NAME:$CONTAINER_TAG"
+			sh "docker push $DOCKER_HUB_USER/$CONTAINER_NAME:$CONTAINER_TAG"
 			echo "Image $IMAGE_NAME push complete"
 		}
 	}
